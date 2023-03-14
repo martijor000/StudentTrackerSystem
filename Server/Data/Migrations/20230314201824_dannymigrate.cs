23b94cc1bf19c28056d670b6ee0971475b0e2092
DannyGuerreroo <dannyguerrerostudent@gmail.com>
2023-03-14 13:43:04 -0700

    Added migration and updated database

---
+++ Server/Data/Migrations/20230314201824_dannymigrate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentTrackerSystem.Server.Data.Migrations
+{
+    public partial class dannymigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
